595398723e1c03d6b00dc41155897c630b95f546
Orchaldir <orchaldir@gmail.com>
2020-03-21 18:09:11 +0100

    [#13] Add TileType & TileMap

--- rusted-arena/examples/map.rs
+++ rusted-arena/examples/map.rs
@@ -1,13 +1,66 @@
 extern crate rusted_tiles;
 
 use rusted_tiles::math::color::*;
+use rusted_tiles::math::get_index;
 use rusted_tiles::rendering::glium_impl::window::GliumWindow;
 use rusted_tiles::rendering::tile::TileRenderer;
 use rusted_tiles::rendering::{App, MouseButton, Renderer, VirtualKeyCode, Window};
 use std::cell::RefCell;
 use std::rc::Rc;
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Floor,
+    Wall,
+}
+
+pub struct TileMap {
+    size: [u32; 2],
+    tiles: Vec<TileType>,
+}
+
+impl TileMap {
+    fn new(size: [u32; 2], default: TileType) -> TileMap {
+        let mut tiles = vec![default; (size[0] * size[1]) as usize];
+
+        for x in 0..size[0] {
+            tiles[get_index([x, 0], size)] = TileType::Wall;
+            tiles[get_index([x, size[1] - 1], size)] = TileType::Wall;
+        }
+
+        for y in 0..size[1] {
+            tiles[get_index([0, y], size)] = TileType::Wall;
+            tiles[get_index([size[0] - 1, y], size)] = TileType::Wall;
+        }
+
+        TileMap { size, tiles }
+    }
+
+    fn render(&mut self, renderer: &mut TileRenderer) {
+        let mut y = 0;
+        let mut x = 0;
+
+        for tile in self.tiles.iter() {
+            match tile {
+                TileType::Floor => {
+                    renderer.add_ascii([x, y], b'.', WHITE);
+                }
+                TileType::Wall => {
+                    renderer.add_ascii([x, y], b'#', WHITE);
+                }
+            }
+
+            x += 1;
+            if x >= self.size[0] {
+                x = 0;
+                y += 1;
+            }
+        }
+    }
+}
+
 pub struct MapApp {
+    map: TileMap,
     pos: [u32; 2],
     tile_renderer: TileRenderer,
 }
@@ -15,7 +68,7 @@ pub struct MapApp {
 impl App for MapApp {
     fn render(&mut self, renderer: &mut dyn Renderer) {
         self.tile_renderer.clear();
-        self.tile_renderer.add_text([0, 0], "Test!", RED);
+        self.map.render(&mut self.tile_renderer);
         self.tile_renderer.add_ascii(self.pos, b'@', RED);
 
         renderer.start(BLACK);
@@ -41,8 +94,10 @@ impl App for MapApp {
 }
 
 fn main() {
-    let mut window = GliumWindow::new("Map Example", [40, 30], [20, 20]);
+    let size = [40, 30];
+    let mut window = GliumWindow::new("Map Example", size, [20, 20]);
     let app = Rc::new(RefCell::new(MapApp {
+        map: TileMap::new(size, TileType::Floor),
         pos: [10, 10],
         tile_renderer: window.get_tile_renderer(),
     }));
--- rusted_tiles/src/math/mod.rs
+++ rusted_tiles/src/math/mod.rs
@@ -1,5 +1,9 @@
 pub mod color;
 
+pub fn get_index(position: [u32; 2], size: [u32; 2]) -> usize {
+    ((position[1] * size[0]) + position[0]) as usize
+}
+
 pub fn get_corners(position: [f32; 2], size: [f32; 2]) -> [[f32; 2]; 4] {
     let corner00 = position;
     let corner10 = [position[0] + size[0], position[1]];
